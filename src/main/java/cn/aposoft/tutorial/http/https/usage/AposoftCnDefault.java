95a0f46a59ad1337f29197cce9c9adb0fd81096d
liujian1 <liujian1@gomeholdings.com>
2017-04-18 20:01:50 +0800

    添加标准的 enable protocols

--- src/main/java/cn/aposoft/tutorial/http/https/usage/AposoftCnDefault.java
+++ src/main/java/cn/aposoft/tutorial/http/https/usage/AposoftCnDefault.java
@@ -11,18 +11,24 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContexts;
 import org.apache.http.ssl.TrustStrategy;
 import org.apache.http.util.EntityUtils;
 
-import cn.aposoft.tutorial.http.https.HttpsTools;
+import cn.aposoft.tutorial.http.https.verifier.DefaultHostnameVerifier;
 
 /**
  * @author LiuJian
@@ -32,12 +38,14 @@ import cn.aposoft.tutorial.http.https.HttpsTools;
 public class AposoftCnDefault {
     public static void main(String[] args)
             throws IOException, KeyManagementException, NoSuchAlgorithmException, KeyStoreException, CertificateException {
+
+        System.setProperty("javax.net.debug", "ssl");
         TrustStrategy trustStrategy = new TrustStrategy() {
             @Override
             public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-                for (X509Certificate cert : chain) {
-                    HttpsTools.visitCertificate(cert);
-                }
+                // for (X509Certificate cert : chain) {
+                // HttpsTools.visitCertificate(cert);
+                // }
                 return false;
             }
             // startcom_certification_authority
@@ -48,14 +56,40 @@ public class AposoftCnDefault {
                 // "changeit".toCharArray(), null)//
                 .loadTrustMaterial(new File("jssecacerts"), "changeit".toCharArray(), trustStrategy) //
                 .build();
+        String[] allProtocols = null;
+        String[] supportedCipherSuites = null;
+        SSLSocketFactory sslFactory = (SSLSocketFactory) sslContext.getSocketFactory();
+        List<String> enabledProtocols = new ArrayList<>();
+        try (SSLSocket sslSocket = (SSLSocket) sslFactory.createSocket();) {
+            allProtocols = sslSocket.getSupportedProtocols();
+            supportedCipherSuites = sslFactory.getSupportedCipherSuites();
+
+            for (final String protocol : allProtocols) {
+                if (!protocol.startsWith("SSL")) {
+                    enabledProtocols.add(protocol);
+                }
+            }
+            System.out.println("ENABLED+PROTOCOLS:" + enabledProtocols);
+        }
+        // cn.aposoft.tutorial.http.https.verifier
+        HostnameVerifier hostNameVerifier = new DefaultHostnameVerifier();
+        SSLConnectionSocketFactory sslSocketFactory;
+        if (allProtocols == null || supportedCipherSuites == null) {
+            sslSocketFactory = new SSLConnectionSocketFactory(sslContext, hostNameVerifier);
+        } else {
+            sslSocketFactory = new SSLConnectionSocketFactory(sslContext, enabledProtocols.toArray(new String[enabledProtocols.size()]),
+                    supportedCipherSuites, hostNameVerifier);
+        }
         try (CloseableHttpClient client = HttpClients.custom()//
                 .setSSLContext(sslContext) //
+                .setSSLSocketFactory(sslSocketFactory)
+
                 .build()) {
-            HttpGet httpGet = new HttpGet("https://www.aposoft.cn/");
+            HttpGet httpGet = new HttpGet("https://www.aposoft.cn:8443/");
             // HttpGet httpGet = new HttpGet("https://www.startssl.com/");
             try (CloseableHttpResponse resp = client.execute(httpGet)) {
                 String respText = EntityUtils.toString(resp.getEntity(), StandardCharsets.UTF_8);
-                System.out.println();
+                System.out.println(respText);
             }
         }
     }

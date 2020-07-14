f335eb2388cc4b7d037b9a38c35f98316d9154f2
Jann.Liu <liujian@9fbank.com.cn>
2020-07-14 10:02:24 +0800

    增加取模运算的解释

--- src/main/java/cn/aposoft/tutorial/crypt/discretelogarithm/ModOperation.java
+++ src/main/java/cn/aposoft/tutorial/crypt/discretelogarithm/ModOperation.java
@@ -5,11 +5,13 @@ package cn.aposoft.tutorial.crypt.discretelogarithm;
 
 /**
  * 模运算公式
+ * 取模运算("Modulus Operation")和取余运算("Remainder Operation ")两个概念有重叠的部分但又不完全一致。
+ * 主要的区别在于对负整数进行除法运算时操作不同。取模主要是用于计算机术语中。取余则更多是数学概念。
  * <pre>
  *   k为常数.
      a = b + kn 记作:,如果a = b + kn,其中k为常数,则 a = b ( mod n);
              如果 k=1, 则 a = b
-             如果 b=1, 则a = 1 ( mod n);
+             如果 b=1, 则a = (1 mod n);
     
  * </pre> 
  *   <pre>
@@ -23,14 +25,4 @@ package cn.aposoft.tutorial.crypt.discretelogarithm;
  * 
  */
 public class ModOperation {
-
-    
-    
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        
-    }
-
 }

5c4555b6fc6ca629fbf26b36a2df56402c00ad10
Bugs <endenvormelody@msn.com>
2020-12-21 23:36:40 +0800

    新增多个sort实例

--- src/main/java/cn/aposoft/tutorial/algorithm/sort/InsertSort.java
+++ src/main/java/cn/aposoft/tutorial/algorithm/sort/InsertSort.java
@@ -3,6 +3,9 @@ package cn.aposoft.tutorial.algorithm.sort;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * 插入排序是一种稳定排序
+ */
 public class InsertSort {
 
     public static void main(String[] args) {
@@ -10,6 +13,23 @@ public class InsertSort {
         Integer[] v = {5, 4, 8, 3, 10, 3, 11, 20, 15, 1};
         List<Integer> vl = Arrays.asList(v);
         sort(v);
+        System.out.println(Arrays.toString(v));
+    }
+
+    // from 算法导论 伪代码从 下标1开始，此处0开始，需要调整
+    //  the algorithms
+    public static void sort1(Integer[] v) {
+        int n = v.length;
+        for (int i = 1; i < n; i++) {
+            int key = v[i];        //复制为哨兵，即存储待排序元素
+            int j = i - 1;
+            //查找在有序表的插入位置
+            while (j > -1 && key < v[j]) {
+                v[j + 1] = v[j];
+                j--;         //元素后移
+            }
+            v[j + 1] = key;      //插入到正确位置
+        }
         System.out.println(Arrays.toString(v));
     }
 
@@ -21,9 +41,9 @@ public class InsertSort {
             if (v[i] < v[i - 1]) {               //若第i个元素大于i-1元素，直接插入。小于的话，移动有序表后插入
                 int j = i - 1;
                 int x = v[i];        //复制为哨兵，即存储待排序元素
-                v[i] = v[i - 1];           //先后移一个元素
+//                v[i] = v[i - 1];           //先后移一个元素
                 //查找在有序表的插入位置
-                while (j>=0 && x < v[j]) {
+                while (j >= 0 && x < v[j]) {
                     v[j + 1] = v[j];
                     j--;         //元素后移
                 }
